ff7aeec832de6d4401e5019bae5cedb441eee54e
Wang AnJun <wanganjun1992@hotmail.com>
2015-05-06 15:39:08 +0800

    对 SDK 做了一些小的改动

--- JumaSDK-iOS-lite-demo/JumaSDK-iOS-lite-demo/JumaDeviceManager/JumaDeviceManager.h
+++ JumaSDK-iOS-lite-demo/JumaSDK-iOS-lite-demo/JumaDeviceManager/JumaDeviceManager.h
@@ -26,7 +26,7 @@ typedef NS_ENUM(NSInteger, JumaDeviceManagerState) {
 
 @property (nonatomic, weak) id<JumaDeviceManagerDelegate> delegate; 
 
-/** step 02, 扫描当前设备周围的其他设备, 传入 nil 则扫描所有设备 */
+/** step 02, 扫描当前设备周围的其他设备 */
 - (void)scanDeviceWithName:(NSString *)deviceName;
 - (void)stopScan;
 
@@ -47,15 +47,14 @@ typedef NS_ENUM(NSInteger, JumaDeviceManagerState) {
 /** step 01, 当前设备上的蓝牙的状态已经改变, 每改变一次, 这个方法就会被 SDK 调用一次 */
 - (void)deviceManager:(JumaDeviceManager *)deviceManager didUpdateState:(JumaDeviceManagerState)state;
 
-- (void)deviceManagerDidStopScan:(JumaDeviceManager *)deviceManager;
-
 /** step 03, 已经发现目标设备 */
 - (void)deviceManager:(JumaDeviceManager *)deviceManager didDiscoverDevice:(NSString *)deviceUUID name:(NSString *)deviceName RSSI:(NSNumber *)RSSI;
+- (void)deviceManagerDidStopScan:(JumaDeviceManager *)deviceManager;
 
 /** step 05, 连接到设备 */
 - (void)deviceManager:(JumaDeviceManager *)deviceManager didConnectDevice:(NSString *)deviceUUID;
 - (void)deviceManager:(JumaDeviceManager *)deviceManager didFailToConnectDevice:(NSString *)deviceUUID error:(NSString *)error;
-/** 如果连接出错断开, error != nil, 否则 error == nil. code 暂未实现 */
+/** 如果连接出错断开, error != nil, 否则 error == nil. code 暂未实现. byRemote 暂未实现 */
 - (void)deviceManager:(JumaDeviceManager *)deviceManager didDisconnectDevice:(NSString *)deviceUUID byRemote:(BOOL)byRemote code:(NSInteger)code error:(NSString *)error;
 
 /** step 07, 接收目标设备返回的数据 */
